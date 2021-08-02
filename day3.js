6799919b6094dffcaefe90226cd1848a8550b78e
igorflyunt <igorflyunt2@gmail.com>
2021-08-02 15:45:15 +0300

    - Created a function 'printForecast' -which takes arrays 'temp1' + 'temp2' and merge to 'temps' -created loop -log - log to console temp[i] + string - 'celsium and day index'

--- day3.js
+++ day3.js
@@ -20,24 +20,81 @@
 // - Find min value in temp array
 // - Subtract min from max (amplitude) and return it
 
-const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+// const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
 
-const calcTempAmlitude = function (temp1, temp2) {
-  let temps = temp1.concat(temp2);
-  let max = temps[0];
-  let min = temps[0];
+// const calcTempAmlitude = function (temp1, temp2) {
+//   let temps = temp1.concat(temp2);
+//   let max = temps[0];
+//   let min = temps[0];
 
-  for (let i = 0; i < temps.length; i++) {
-    const curTemp = temps[i];
+//   for (let i = 0; i < temps.length; i++) {
+//     const curTemp = temps[i];
 
-    if (typeof curTemp !== 'number') continue;
-    if (curTemp > max) max = curTemp;
-    if (curTemp < min) min = curTemp;
-  }
-  console.log(max, min);
-  return max - min;
-};
-const amplitude = calcTempAmlitude([3, 5, 1], [6, 4, 9]);
-console.log(amplitude);
+//     if (typeof curTemp !== 'number') continue;
+//     if (curTemp > max) max = curTemp;
+//     if (curTemp < min) min = curTemp;
+//   }
+//   console.log(max, min);
+//   return max - min;
+// };
+// const amplitude = calcTempAmlitude([3, 5, 1], [6, 4, 9]);
+// console.log(amplitude);
 
 ////////////////////////////////////////////////////////
+///////////////////////////////////////
+// Coding Challenge #1
+
+/*
+Given an array of forecasted maximum temperatures, the thermometer displays 
+a string with these temperatures.
+
+Example: [17, 21, 23] will print "... 17ºC in 1 days ... 21ºC in 2 days ...
+ 23ºC in 3 days ..."
+
+Create a function 'printForecast' which takes in an array 'arr' and logs
+ a string like the above to the console.
+
+Use the problem-solving framework: Understand the problem and break 
+it up into sub-problems!
+
+TEST DATA 1: [17, 21, 23]
+TEST DATA 2: [12, 5, -5, 0, 4]
+*/
+
+// 1) Understanding the problem
+// Need to create 2 arrays
+// Create function wich takes element in array and conver to string
+// Add to string +  "ºC" + in 1 days (day number)
+// Write information on console
+
+// 2) Breaking up into sub-problems
+// create func which takes 2 arr
+// concat 2 arr in 1 arr
+// loop for to 1rr.lenght where lenght will day number
+// concat this string in loop to console
+
+// 1) Understanding the problem
+// - Array transformed to string, separated by ...
+// - What is the X days? Answer: index + 1
+
+// 2) Breaking up into sub-problems
+// - Transform array into string
+// - Transform each element to string with ºC
+// - Strings needs to contain day (index + 1)
+// - Add ... between elements and start and end of string
+// - Log string to console
+
+const temp1 = [17, 21, 23];
+const temp2 = [12, 5, -5, 0, 4];
+
+const printForecast = function (temp1, temp2) {
+  temps = temp1.concat(temp2);
+
+  for (let i = 0; i < temps.length; i++) {
+    let days = i + 1;
+    console.log(`${temps[i]}ºC in ${days} days`);
+  }
+};
+
+const forecast = printForecast(temp1, temp2);
+console.log(forecast);
