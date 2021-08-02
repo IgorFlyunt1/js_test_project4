e63fd345cce7468e5e6442f8ed6305463905b157
igorflyunt <igorflyunt2@gmail.com>
2021-08-02 14:39:15 +0300

    -added 2 values in input to function (calcTempAmlitude) -merge 2 input values in one array

--- day3.js
+++ day3.js
@@ -22,12 +22,14 @@
 
 const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
 
-const calcTempAmlitude = function (temps) {
+const calcTempAmlitude = function (temp1, temp2) {
+  let temps = temp1.concat(temp2);
   let max = temps[0];
   let min = temps[0];
 
   for (let i = 0; i < temps.length; i++) {
     const curTemp = temps[i];
+
     if (typeof curTemp !== 'number') continue;
     if (curTemp > max) max = curTemp;
     if (curTemp < min) min = curTemp;
@@ -35,7 +37,7 @@ const calcTempAmlitude = function (temps) {
   console.log(max, min);
   return max - min;
 };
-const amplitude = calcTempAmlitude([3, 5, 7]);
+const amplitude = calcTempAmlitude([3, 5, 1], [6, 4, 9]);
 console.log(amplitude);
 
 ////////////////////////////////////////////////////////
